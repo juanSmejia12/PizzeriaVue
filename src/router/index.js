fe655d61bbaf96a4dbe7afaff22479d9c1a2392f
Juan Quiñonez <juandavid9812@gmail.com>
2025-05-27 22:57:35 -0500

    Actualizacion de las rutas de algunas vistas

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,16 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+
+
+import HomeView from '../views/HomeView.vue';
+import Purchases from '../views/Purchases.vue';
+import Clients from '@/views/Clients.vue';
+import Order_extra_ingredients from '@/views/Order_extra_ingredients.vue';
+import Pizza_raw_materials from '@/views/Pizza_raw_materials.vue';
+import Raw_material from '@/views/Raw_material.vue';
+import Branches from '@/views/Branches.vue';
+import App from '@/views/App.vue';
+
 
 const routes = [
   {
@@ -8,8 +19,49 @@ const routes = [
     component: HomeView
   },
   {
+    path: '/clients',
+    name: 'clients',
+    component: Clients
+  },
+
+  {
+    path: '/purchases',
+    name: 'purchases',
+    component: Purchases
+  },
+
+  {
+    path: '/app',
+    name: 'app',
+    component: App
+  },
+{
+    path: '/pizza_raw_materials',
+    name: 'pizza_raw_materials',
+    component: Pizza_raw_materials
+  },
+
+  {
+    path: '/branches',
+    name: 'branches',
+    component: Branches
+  },
+
+  {
+    path: '/raw_material',
+    name: 'raw_material',
+    component: Raw_material
+  },
+
+   {
+    path: '/order_extra_ingredients',
+    name: 'order_extra_ingredients',
+    component: Order_extra_ingredients
+  },
+  {
     path: '/about',
     name: 'about',
+    
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
